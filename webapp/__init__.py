d55495aee527d0ce4dbfcbdb64c32e81a75e3ead
39r2d4 <telepuzik.blg@gmial.com>
2023-06-02 22:48:40 +0200

    добавил flask_wtf, форму страницы авторизации.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, url_for
 
+from webapp.forms import LoginForm
 from webapp.model import db
 
 def create_app():
@@ -12,5 +13,11 @@ def create_app():
     @app.route("/")
     def hello():
         return render_template("index.html", number = 5)
+    
+
+    @app.route("/login")
+    def login():
+        login_form = LoginForm()
+        return render_template("login.html", form=login_form)
 
     return app
--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,4 @@
 from webapp.settings import DATABASE_URI
 
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
+SECRET_KEY = "this_is_secret_key_you_must_change_it"
