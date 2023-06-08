82d2614a129b6b4e2058d13c9a61625cc7aa67b8
39r2d4 <z67500@yandex.ru>
2023-06-08 12:37:39 +0200

    Добавлен путь выхода, и обработка повторного захода на страницу авторизации. (#6)
    Co-authored-by: 39r2d4 <telepuzik.blg@gmail.com>

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, flash, url_for
-
+from flask import Flask, render_template, flash, url_for, redirect
+from flask_login import LoginManager, current_user, login_user, logout_user
 from webapp.forms import LoginForm
 from webapp.model import db
 
@@ -17,7 +17,16 @@ def create_app():
 
     @app.route("/login")
     def login():
+        if current_user.is_authenticated:
+            return(redirect(url_for("index")))
         login_form = LoginForm()
         return render_template("login.html", form=login_form)
+
+
+    app.route("/logout")
+    def logout():
+        logout_user()
+        flash("Вы вышли из системы")
+        return url_for("index")
 
     return app
