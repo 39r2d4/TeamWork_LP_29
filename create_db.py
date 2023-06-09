abd384010d5474c16d5ce2faf1e97b2d96e9caf2
39r2d4 <z67500@yandex.ru>
2023-06-09 23:39:10 +0200

    Feature/user login (#5)
    * Добавил к форме login функционал аторизации
    
    * EOF, + мелкие ошибки
    
    * удалил принт, изменил create_admin.py
    
    * опечатки
    
    ---------
    
    Co-authored-by: 39r2d4 <telepuzik.blg@gmail.com>

--- create_db.py
+++ create_db.py
@@ -4,11 +4,7 @@ from webapp.model import User
 
 app = create_app()
 
-test_user = User(name = "First User")
-
 # c какой-то версии алхимия перестала принимать app в reate_all() 
 with app.app_context():
     db.create_all() 
-    db.session.add(test_user)
     db.session.commit()
-
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,22 +1,55 @@
-from flask import Flask, render_template, flash, url_for, redirect
-from flask_login import LoginManager, current_user, login_user, logout_user
+from flask import Flask, request, render_template, flash, url_for, redirect
+from flask_login import current_user, LoginManager, login_user, ogout_user
 from webapp.forms import LoginForm
-from webapp.model import db
+from webapp.model import db, User
+
+
 
 def create_app():
 
     app  = Flask(__name__)
     app.config.from_pyfile("config.py")
+
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
 
     @app.route("/")
-    def hello():
+    def index():
         return render_template("index.html", number = 5)
     
 
-    @app.route("/login")
+    @app.route("/login", methods=["POST", "GET"])
     def login():
+
+        if request.method == "GET":
+            login_form = LoginForm()
+            return render_template("login.html", form=login_form)
+        
+        elif request.method == "POST":
+            login_form = LoginForm()
+            if login_form.validate_on_submit():
+                try:
+                    user = User.query.filter_by(username=login_form.username.data).first()
+                    if user and user.check_password(login_form.password.data):
+                        login_user(user)
+                        flash('Успешная авторизация')
+                        return redirect(url_for("index"))
+                except: #"sqlalchemy.exc.OperationalError" Добавить обработку ошибок алхимии (!!!)
+                    flash("БД недоступна, повторите попытку позже")
+                    return redirect(url_for("login"))
+
+            flash("Не правильный логин или пароль")
+            return redirect(url_for("login"))
+
         if current_user.is_authenticated:
             return(redirect(url_for("index")))
         login_form = LoginForm()
