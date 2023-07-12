3335eb7354f3ef7317f80c39cfd7b3e28ba1fd79
Darmidon5 <119671980+Darmidon5@users.noreply.github.com>
2023-07-12 21:43:08 +0300

    Feature beautiful (#16)
    * few changes
    
    * few changes
    
    * few changes
    
    * few changes
    
    * pep8 refactoring
    
    * удалал токен
    
    ---------
    
    Co-authored-by: Anton Topchii <topchiy.anton@gmail.com>
    Co-authored-by: 39r2d4 <telepuzik.blg@gmail.com>

--- webapp/deck/models.py
+++ webapp/deck/models.py
@@ -5,7 +5,7 @@ class Deck(db.Model):
     __tablename__ = "deck_table"
     id: db.Mapped[int] = db.mapped_column(db.Integer, primary_key=True)
     name: db.Mapped[str] = db.mapped_column(db.String(64), index=True)
-    comment: db.Mapped[str] = db.mapped_column(db.String(128))
+    comment: db.Mapped[str] = db.mapped_column(db.String(128), nullable=True, default='')
     user_id: db.Mapped[int] = db.mapped_column(db.ForeignKey("user_teble.id"), index=True)
 
     user: db.Mapped["User"] = db.relationship(back_populates="deck")
