224802eb6aecf3fe24207b80a07de6e97856700b
39r2d4 <telepuzik.blg@gmail.com>
2023-07-21 10:03:53 +0200

    добавление тестов(проверка new_deck)

--- tests/deck/test_deck_views.py
+++ tests/deck/test_deck_views.py
@@ -14,7 +14,7 @@ def test_decks_add_new_deck_page(client, auth):
     response = client.get('decks/new')
     assert response.status_code == 200
     #Имя новой колоды:
-    assert b'\xd0\x98\xd0\xbc\xd1\x8f \xd0\xbd\xd0\xbe\xd0\xb2\xd0\xbe\xd0\xb9 \xd0\xba\xd0\xbe\xd0\xbb\xd0\xbe\xd0\xb4\xd1\x8b' in response.data
+    assert '<span>Имя новой колоды:</span>'.encode('UTF-8')  in response.data
     auth.logout()
     response = client.get('/decks/new')
     assert response.status_code == 302
