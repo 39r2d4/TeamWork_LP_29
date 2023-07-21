a7e839b6ac6d25760903e80c996c0f542b565d8d
39r2d4 <telepuzik.blg@gmail.com>
2023-07-21 09:32:07 +0200

    добавление тестов

--- tests/card/test_card_views.py
+++ tests/card/test_card_views.py
@@ -1,13 +1,11 @@
-from flask import session
-
 def test_create_card_page(client, auth):
     with client:
         auth.login()
         response = client.get('/cards/new')
         print(response)
-        assert '<label for="side_1">Лицо</label>'.encode('UTF-8') in response.data
         assert response.status_code == 200
+        assert '<label for="side_1">Лицо</label>'.encode('UTF-8') in response.data
 
-
-    
-    
+        auth.logout()
+        response = client.get('/cards/new')
+        assert response.status_code == 302
--- tests/deck/test_deck_views.py
+++ tests/deck/test_deck_views.py
@@ -1,3 +1,9 @@
-def test_decks_view_page(client):
+def test_decks_view_page(client, auth):
+    auth.login()
     response = client.get('/decks/view')
+    assert response.status_code == 200
     assert b'class="list-group-item list-group-item-action"' in response.data
+    
+    auth.logout()
+    response = client.get('/decks/view')
+    assert response.status_code == 302
