18446df8f726e487e20887b228551cf80ee97c21
39r2d4 <z67500@yandex.ru>
2023-08-20 17:40:26 +0200

    Feature add card from file (#32)
    * add upload cards from file form
    
    * add route, add work prototype
    
    * add exception, add flash info
    
    * add modal form on teamplate
    
    * add func cards_from_workbook
    
    * add EOF in card/view.py
    
    ---------
    
    Co-authored-by: 39r2d4 <telepuzik.blg@gmail.com>

--- webapp/card/views.py
+++ webapp/card/views.py
@@ -2,13 +2,18 @@ from datetime import datetime
 
 from flask import Blueprint, render_template, flash, url_for, redirect
 from flask_login import current_user, login_required
-from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import OperationalError, SQLAlchemyError
 
+from openpyxl import load_workbook, Workbook
+from openpyxl.utils.exceptions import InvalidFileException
 
-from webapp.card.forms import BaseCardForm, NewCardForm
+from zipfile import BadZipFile
+
+from webapp.card.forms import BaseCardForm, CardsFromFile, NewCardForm 
 from webapp.model import db
 from webapp.card.models import Card, CardType
 from webapp.deck.views import create_list_of_decks
+from werkzeug.utils import secure_filename
 
 from webapp.config import OPERATIONALERROR_TEXT
 blueprint = Blueprint('card', __name__, url_prefix='/cards')
@@ -24,6 +29,34 @@ def delete_card(card):
     except OperationalError:
         flash(OPERATIONALERROR_TEXT)
         return OPERATIONALERROR_TEXT
+    
+
+# стоит подумать передавать форму целиком или данные из формы    
+def cards_list_from_workbook(workbook, data_from_form):
+    now = datetime.now().date()
+    cards_list = []
+
+    for row in workbook.rows:
+        data_from_row = []
+        for cell in row:
+            data_from_row.append(cell.value)
+        card = {"side_1": data_from_row[0], 
+                "side_2": data_from_row[1], 
+                "deck_id": data_from_form["deck_id"], 
+                "is_active": True, 
+                "tags": "card from file",
+                "cardtype_id": data_from_form["cardtype_id"], 
+                "user_id": current_user.id,
+                "weights": 2.5,
+                "inter_repetition_interval": 0,
+                "successfully_count": 0,
+                "last_repetition": now,
+                "next_repetition": now
+        }
+        cards_list.append(card)
+    cards_list.pop(0)
+    return cards_list
+
 
 
 @blueprint.route("/new", methods=["POST", "GET"])
@@ -31,7 +64,10 @@ def delete_card(card):
 def create_card():
     try:
         card_form = NewCardForm()
+        cards_from_file = CardsFromFile()
+
         if card_form.validate_on_submit():
+            #вынести в функцию !!!
             now = datetime.now().date()
             new_card = Card(
                 side_1=card_form.side_1.data,
@@ -65,7 +101,11 @@ def create_card():
         card_form.deck.choices = decks
         card_form.type.choices = card_types
 
-        return render_template("card/add_new_card_form.html", card_form=card_form, decks=create_list_of_decks(), page_title='создание карточки')
+        cards_from_file.deck.choices = decks
+        cards_from_file.type.choices = card_types
+
+
+        return render_template("card/add_new_card_form.html", card_form=card_form,  cards_from_file=cards_from_file , page_title='создание карточки')# откуда тут взялось decks=create_list_of_decks()?
 
     except OperationalError:
         flash(OPERATIONALERROR_TEXT)
@@ -120,4 +160,33 @@ def edit_card(card_id):
         flash(OPERATIONALERROR_TEXT)
         return OPERATIONALERROR_TEXT
 
+#Пока пихаем все в одну функцию, потом уже разнести
+@blueprint.route("/from_file", methods=["POST"])
+@login_required
+def load_cards_from_file():
+    cards_from_file = CardsFromFile()
+    if cards_from_file.validate_on_submit():
+        file_name = cards_from_file.file_with_cards.data.filename 
 
+        if file_name.split(".")[-1] in ["xlsx", "xls"]:
+            try:
+                workbook = load_workbook(filename=cards_from_file.file_with_cards.data)
+                ws = workbook.active
+                
+                date_from_from = {"cardtype_id": cards_from_file.type.data, "deck_id": cards_from_file.deck.data}
+
+                cards_list = cards_list_from_workbook(ws, date_from_from)
+
+                db.session.bulk_insert_mappings(Card, cards_list, return_defaults=True)
+                db.session.commit()
+                flash("Карточки успешно созданы", "info")
+
+            except(SQLAlchemyError):
+                flash("Ошибка записи данных", "error")
+            except(BadZipFile, InvalidFileException):
+                flash("Ошибка чтения файла", "error")
+        else:
+            flash("Файл не xls | xlsx", "warning")
+    else:
+        flash("Ошибка в заполнении формы", "error")    
+    return(redirect(url_for("card.create_card")))
--- webapp/config.py
+++ webapp/config.py
@@ -4,3 +4,4 @@ SECRET_KEY = "this_is_secret_key_you_must_change_it"
 OPERATIONALERROR_TEXT = "БД недоступна, повторите попытку позже"
 RECAPTCHA_PUBLIC_KEY = public
 RECAPTCHA_PRIVATE_KEY = private
+MAX_CONTENT_LENGTH = 5_0000_0000
